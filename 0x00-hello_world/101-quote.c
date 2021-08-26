387185449eb98c0db6d52d1e6cf8bce8647b1aee
peruth <peruth98@gmail.com>
2021-08-26 09:22:51 -0700

    string prints to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - prints exactly "and that piece of art is useful"
+ *      - Dora Korpar, 2015-10-19
+ * followed by the new line to the standard error
+ * Return: Always 0 on (success)
+ */
+
 int main(void)
 {
-/*prints a string to the standard error*/
-write(3, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",50);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
