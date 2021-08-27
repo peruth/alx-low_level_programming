c8e1be42352774d347d7645fba34e3216095d97e
peruth <peruth98@gmail.com>
2021-08-27 13:13:58 -0700

    program that prints all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print single digit number dtarting from 0 using putchar
+ * main - print single digit number starting from 0 using putchar
  * followed by a newline
  * Return: Always 0 on (success)
  */
