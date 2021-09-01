0c31ad581ff9036ceaf8356b579d2222f830eaae
peruth <peruth98@gmail.com>
2021-09-01 13:38:44 -0700

    print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "mainn.h"
 
 /**
  * main - prints Holberton, followed by a newline
@@ -7,14 +7,15 @@
 
 int main(void)
 {
-_putchar(');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
 _putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
 return (0);
 }
