762c5b0cd5e8566b723db013e0b92768feaac2d1
peruth <peruth98@gmail.com>
2021-09-01 13:16:10 -0700

    print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,19 +2,21 @@
 
 /**
  * main - prints Holberton, followed by a newline
- * Return: Always 0 (success)
+ * Return: Always 0 on (success)
  */
+
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-return (0);
+	return (0);
 }
