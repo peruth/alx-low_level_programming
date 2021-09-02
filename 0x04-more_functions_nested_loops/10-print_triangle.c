b287e5635219ba0157120f4abc7d4417974c95bc
peruth <peruth98@gmail.com>
2021-09-02 07:01:37 -0700

    prime factors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,9 +8,9 @@ void print_triangle(int size)
 	int row, column, k;
 
 	if (size <= 0)
-	  {
+
 		_putchar('\n');
-	  }
+
 	for (row = 0; row < size; row++)
 	{
 		for (column = size - row; column > 1; column--)
