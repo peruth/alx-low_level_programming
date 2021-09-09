6d86d023e5b00a40f066a5ce8cacc6850c500ecf
peruth <peruth98@gmail.com>
2021-09-09 07:05:54 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,20 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
 
-for (j = 0; dest[j] != '\0'; j++)
+i = 0;
+j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0' && j < n)
 {
-for (i = 0; src[i] != '\0'; i++)
-{
-if (src[i] < n)
-{
-dest[j] = src[i];
+dest[i] = src[j];
+i++;
+j++;
 }
-}
-dest[j] = '\0';
-}
+
+dest[i] = '\0';
+
 return (dest);
 }
