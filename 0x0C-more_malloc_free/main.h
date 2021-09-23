385d35ff37c104346d99d879b24db4715b6e618f
peruth <peruth98@gmail.com>
2021-09-23 14:54:47 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,8 @@ int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
+int _strlen(char *s)
+void errors(void);
+int is_digit(char *s);
 
 #endif
